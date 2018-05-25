52095726c26005b45680db87dcd9cc9435266fc2
Hari Gillala <46038737@ad.mmu.ac.uk>
2018-05-25 11:21:19 +0100

    linked from git

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Deployed to Azure</h1>
+        <h1>Deployed to Azure and linked from Git</h1>
       </div>
     );
   }
